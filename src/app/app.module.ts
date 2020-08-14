cf19a4e9fb8c615e8d5cb6371683f886d3206677
Jide Lambo <jide.lambo@nordcloud.com>
2020-08-15 01:55:00 +0300

    Add playground

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Component } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { RecentWorkComponent } from './components/recent-work/recent-work.component';
-import { FooterSocialComponent } from './components/footer-social/footer-social.component';
-import { AboutComponent } from './pages/about/about.component';
-import { HomeBriefsComponent } from './components/home-briefs/home-briefs.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { WorksHeroComponent } from './components/works-hero/works-hero.component';
-import { WorkGigmComponent } from './pages/work-gigm/work-gigm.component';
-import { WorkGtpayComponent } from './pages/work-gtpay/work-gtpay.component';
-import { WorkShareitComponent } from './pages/work-shareit/work-shareit.component';
-import { DesignProcessComponent } from './pages/design-process/design-process.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { WorkCouponcoolerComponent } from './pages/work-couponcooler/work-couponcooler.component';
-import { WorkMicrofinanceComponent } from './pages/work-microfinance/work-microfinance.component';
-import { WorkBcasterComponent } from './pages/work-bcaster/work-bcaster.component';
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { RecentWorkComponent } from "./components/recent-work/recent-work.component";
+import { FooterSocialComponent } from "./components/footer-social/footer-social.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { HomeBriefsComponent } from "./components/home-briefs/home-briefs.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { WorksHeroComponent } from "./components/works-hero/works-hero.component";
+import { WorkGigmComponent } from "./pages/work-gigm/work-gigm.component";
+import { WorkGtpayComponent } from "./pages/work-gtpay/work-gtpay.component";
+import { WorkShareitComponent } from "./pages/work-shareit/work-shareit.component";
+import { DesignProcessComponent } from "./pages/design-process/design-process.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { WorkCouponcoolerComponent } from "./pages/work-couponcooler/work-couponcooler.component";
+import { WorkMicrofinanceComponent } from "./pages/work-microfinance/work-microfinance.component";
+import { WorkBcasterComponent } from "./pages/work-bcaster/work-bcaster.component";
+import { PlaygroundComponent } from "./pages/playground/playground.component";
 
 const appRoutes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'GIGM', component: WorkGigmComponent },
-  { path: 'gtpay', component: WorkGtpayComponent },
-  { path: 'shareit', component: WorkShareitComponent },
-  { path: 'design-process', component: DesignProcessComponent },
-  { path: 'couponcooler', component: WorkCouponcoolerComponent },
-  { path: 'microfinance', component: WorkMicrofinanceComponent },
-  { path: 'bcaster-dashboard', component: WorkBcasterComponent },
-  { path: 'not-found', component: PageNotFoundComponent},
-  { path: '**', redirectTo: 'not-found'}
+  { path: "", component: HomePageComponent },
+  { path: "about", component: AboutComponent },
+  { path: "playground", component: PlaygroundComponent },
+  { path: "GIGM", component: WorkGigmComponent },
+  { path: "gtpay", component: WorkGtpayComponent },
+  { path: "shareit", component: WorkShareitComponent },
+  { path: "design-process", component: DesignProcessComponent },
+  { path: "couponcooler", component: WorkCouponcoolerComponent },
+  { path: "microfinance", component: WorkMicrofinanceComponent },
+  { path: "bcaster-dashboard", component: WorkBcasterComponent },
+  { path: "not-found", component: PageNotFoundComponent },
+  { path: "**", redirectTo: "not-found" },
 ];
 
 @NgModule({
@@ -55,17 +56,17 @@ const appRoutes: Routes = [
     WorkCouponcoolerComponent,
     WorkMicrofinanceComponent,
     WorkBcasterComponent,
+    PlaygroundComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    ),
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    ServiceWorkerModule.register("/ngsw-worker.js", {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,28 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
 export class AboutComponent implements OnInit {
-
   skills: Object[] = [
-    { name: 'Research techniques.' },
-    { name: 'Flow diagrams & Wireframes.' },
-    { name: 'User Interface design.' },
-    { name: 'Sketching & Prototyping.' },
-    { name: 'HTML5, CSS3, JavaScript.' },
-    { name: 'Mobile & Responsive design.' },
-    { name: 'Communication and customer facing skills.' },
-    { name: 'Proficient with Sketchapp and other prototyping tools.' },
-    { name: 'Github and BitBucket version control.' }
+    { name: "Research techniques." },
+    { name: "Flow diagrams & Wireframes." },
+    { name: "User Interface development: HTML5, CSS3, JavaScript" },
+    { name: "Sketching & Prototyping." },
+    { name: "Mobile & Responsive design." },
+    { name: "Proficient with Sketchapp and other prototyping tools." },
+    { name: "Github version control." },
   ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    console.log('about page here');
+    console.log("about page here");
   }
-
 }
