2410dc3fc1a3ef0130fc715aa85f7974594c60f6
Jide Lambo <jide.lambo@nordcloud.com>
2020-09-09 00:02:10 +0300

    UI changes to my latest work

--- src/app/components/recent-work/recent-work.component.ts
+++ src/app/components/recent-work/recent-work.component.ts
@@ -1,38 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-recent-work',
-  templateUrl: './recent-work.component.html',
-  styleUrls: ['./recent-work.component.scss']
+  selector: "app-recent-work",
+  templateUrl: "./recent-work.component.html",
+  styleUrls: ["./recent-work.component.scss"],
 })
 export class RecentWorkComponent implements OnInit {
-
   works: Object[] = [
-    { url: 'bcaster-dashboard',
-    name: 'BCaster OY, Finland',
-    desc: 'First scalable, channel-independent platform for brands to activate and engage users.',
-    img: 'ICC-Dashboard-banner.png',
-    color: '#162447' },
-    { url: 'GIGM',
-    name: 'GIGM.com',
-    desc: 'Changing the road transport system approach in Nigeria',
-    img: 'gigm-web.png',
-    color: 'rgb(250, 211, 224)' },
-    { url: 'gtpay',
-    name: 'GTPay',
-    desc: 'Payment solution\'s redesign for better user\'s experience',
-    img: 'gtpay-mobile.png',
-    color: '#a2c8d8' },
-    { url: 'couponcooler',
-    name: 'Coupon Cooler',
-    desc: 'Coupon deals, and discount for shoppers on mobile experience',
-    img: 'couponcooler.png',
-    color: '#fdf182' },
+    {
+      url: "bcaster-dashboard",
+      name: "BCaster OY, Finland",
+      desc:
+        "First scalable, channel-independent platform for brands to activate and engage users.",
+      img: "ICC-Dashboard-banner.png",
+      color: "#162447",
+    },
+    {
+      url: "GIGM",
+      name: "GIGM.com",
+      desc: "Changing the road transport system approach in Nigeria",
+      img: "gigm-web.png",
+      color: "rgb(250, 211, 224)",
+    },
+    {
+      url: "couponcooler",
+      name: "Coupon Cooler",
+      desc: "Coupon deals, and discount for shoppers on mobile experience",
+      img: "couponcooler.png",
+      color: "#fdf182",
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
