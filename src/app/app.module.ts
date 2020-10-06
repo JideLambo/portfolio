d5a2b23847e20914ad34b2635439affb01cfe461
Jide Lambo <jide.lambo@nordcloud.com>
2020-10-07 00:33:19 +0300

    portfolio update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,11 @@ import { HomeBriefsComponent } from "./components/home-briefs/home-briefs.compon
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { WorksHeroComponent } from "./components/works-hero/works-hero.component";
 import { WorkGigmComponent } from "./pages/work-gigm/work-gigm.component";
-import { WorkGtpayComponent } from "./pages/work-gtpay/work-gtpay.component";
-import { WorkShareitComponent } from "./pages/work-shareit/work-shareit.component";
 import { DesignProcessComponent } from "./pages/design-process/design-process.component";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 import { WorkCouponcoolerComponent } from "./pages/work-couponcooler/work-couponcooler.component";
-import { WorkMicrofinanceComponent } from "./pages/work-microfinance/work-microfinance.component";
 import { WorkBcasterComponent } from "./pages/work-bcaster/work-bcaster.component";
 import { PlaygroundComponent } from "./pages/playground/playground.component";
 
@@ -28,11 +25,8 @@ const appRoutes: Routes = [
   { path: "about", component: AboutComponent },
   { path: "playground", component: PlaygroundComponent },
   { path: "GIGM", component: WorkGigmComponent },
-  { path: "gtpay", component: WorkGtpayComponent },
-  { path: "shareit", component: WorkShareitComponent },
   { path: "design-process", component: DesignProcessComponent },
   { path: "couponcooler", component: WorkCouponcoolerComponent },
-  { path: "microfinance", component: WorkMicrofinanceComponent },
   { path: "bcaster-dashboard", component: WorkBcasterComponent },
   { path: "not-found", component: PageNotFoundComponent },
   { path: "**", redirectTo: "not-found" },
@@ -49,12 +43,9 @@ const appRoutes: Routes = [
     HomePageComponent,
     WorksHeroComponent,
     WorkGigmComponent,
-    WorkGtpayComponent,
-    WorkShareitComponent,
     DesignProcessComponent,
     PageNotFoundComponent,
     WorkCouponcoolerComponent,
-    WorkMicrofinanceComponent,
     WorkBcasterComponent,
     PlaygroundComponent,
   ],
